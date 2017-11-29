69e711dfe7ee19f58f1aab3bb4f77c635c553808
zhengyanbiao <zhengyanbiao@atmb.org>
2017-11-29 09:40:48 +0800

    提交

--- Code/WebUI/Content/JS/GA/base.js
+++ Code/WebUI/Content/JS/GA/base.js
@@ -1,5 +1,106 @@
-﻿
-(function (j) {
+﻿(function (j) {
+    var noop = function () { },
+        guid = 0,
+        index = 1989;
+    window.dj = window.dj || {
+        version: "V2.0.0",
+        mgr: {},
+        error: [],
+        split: /[^, ]+/g,
+        noop: noop,
+        href: "javascript:void(0)",
+        ie6: window.VBArray && !window.XMLHttpRequest,
+        addCmp: function (cmp) {
+            if (!dj.mgr[cmp.id]) { dj.mgr[cmp.id] = cmp; };
+        },
+        getCmp: function (id) {
+            return dj.mgr[id] || null;
+        },
+        deleteCmp: function (id) {
+            var cmp = dj.mgr[id];
+            if (cmp) { delete dj.mgr[id]; }
+        },
+        hideCmp: function (type) {
+            for (var i in dj.mgr) {
+                var cmp = dj.mgr[i];
+                (cmp instanceof type) && cmp.hide && cmp.hide();
+            }
+        },
+        id: function () {
+            return "dj-" + guid++;
+        },
+        zIndex: function () {
+            return index++;
+        },
+        getClass: function () {
+            return function () {
+                this.init.apply(this, arguments);
+            }
+        },
+        extend: function (parent, children, overrides) {
+            //控件继承
+            if (typeof (parent) != "function") { return children; };
+            children.base = parent.prototype;
+            children.base.constructor = parent;
+            var target = noop;
+            target.prototype = parent.prototype;
+            children.prototype = new target();
+            children.prototype.constructor = children;
+            overrides && j.extend(children.prototype, overrides);
+            return children;
+        },
+        control: function (defaults, options) {
+            //控件基类            
+            this.setOptions(defaults, options);
+            this.init && this.init();
+            this.render && this.render();
+            this.loaded && this.loaded();
+            dj.addCmp(this);
+        },
+        destroy: function () {
+            //控件销毁
+            for (var id in dj.mgr) {
+                var cmp = dj.getCmp(id);
+                cmp.destroy && cmp.destroy();
+                delete dj.mgr[id];
+            }
+            delete dj.mgr;
+        }
+    };
+    j.extend(dj.control.prototype, {
+        events: {},
+        getType: function () {
+            return "dj.control";
+        },
+        setOptions: function (defaults, options) {
+            this.type = this.getType();
+            this.id = dj.id();
+            j.extend(this, defaults, options);
+        },
+        fire: function (type) {
+            //未完....
+        },
+        on: function (type, fn) {
+            this.events[type] = fn;
+            //未完....
+        },
+        hasEvent: function (type) {
+            return this.events[type] === undefined;
+        },
+        unon: function (type) {
+            if (type) {
+                this.hasEvent(type) && delete this.events[type];
+            } else {
+                //未完....
+            }
+        },
+        destroy: function () { }
+    });
+    //执行销毁
+    j(window).bind("unload", dj.destroy);
+    try {
+        document.execCommand("BackgroundImageCache", false, true);
+    } catch (e) { }
     //---------------------------------string-prototype-extend---------------------------------
     j.extend(String.prototype, {
         trim: function () {
